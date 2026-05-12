e1295ad77d3854ba067e94c152489e96fa5cf81a
Josh Knight <joshknight2493@gmail.com>
2026-05-12 10:34:27 -0600

    push update

--- server/data/weapons/scripts/burst_arrow.lua
+++ server/data/weapons/scripts/burst_arrow.lua
@@ -2,22 +2,43 @@ local combat = Combat()
 combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
 combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_EXPLOSIONAREA)
 combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_BURSTARROW)
-combat:setParameter(COMBAT_PARAM_BLOCKARMOR, true)
+combat:setParameter(COMBAT_PARAM_BLOCKARMOR, false)
 
 local area = createCombatArea({
-	{1, 1, 1},
-	{1, 3, 1},
-	{1, 1, 1}
+	{0, 0, 1, 1, 1, 0, 0},
+	{0, 1, 1, 1, 1, 1, 0},
+	{1, 1, 1, 1, 1, 1, 1},
+	{1, 1, 1, 3, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 1},
+	{0, 1, 1, 1, 1, 1, 0},
+	{0, 0, 1, 1, 1, 0, 0}
 })
 combat:setArea(area)
 
-function onGetFormulaValues(cid, level, maglevel)
-	min = 0
-	max = -((level * 2) + (maglevel * 3)) * 0.6
+local BURST_ARROW_ATTACK = 37
+
+local function getDamageFactor(attackFactor)
+	if attackFactor >= 1.9 then
+		return 0.5
+	elseif attackFactor >= 1.1 then
+		return 0.75
+	end
+
+	return 1.0
+end
+
+function onGetFormulaValues(player, skill, attack, attackFactor)
+	local damageFactor = getDamageFactor(attackFactor)
+	local level = player:getLevel()
+	local distanceSkill = player:getSkillLevel(SKILL_DISTANCE)
+	local attackValue = BURST_ARROW_ATTACK
+
+	min = -((level / 5) * damageFactor)
+	max = -((((distanceSkill * attackValue * 0.09 * damageFactor) + level) / 5))
 	return min, max
 end
 
-setCombatCallback(combat, CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
+setCombatCallback(combat, CALLBACK_PARAM_SKILLVALUE, "onGetFormulaValues")
 
 function onUseWeapon(player, variant)
 	return combat:execute(player, variant)
